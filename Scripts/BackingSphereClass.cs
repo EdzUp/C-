34b6eb2e2953e5fb9f61282926526225b790f263
Ed <edzupdev@gmail.com>
2017-11-03 16:24:33 +0000

    Add files via upload

---
+++ Scripts/BackingSphereClass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class BackingSphereClass : MonoBehaviour {
+	public Camera Parent;
+	Vector3 Vec3;
+
+	// Use this for initialization
+	void Start () {
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+		mesh.triangles = mesh.triangles.Reverse().ToArray();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Parent.transform.position;
+	}
+}
---
+++ Scripts/Platform.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+static public class Platform {
+	static int SelectedControl;
+	static public bool touchScreen;
+
+	static public void Initialise() {
+		SelectedControl = 0;
+		#if UNITY_ANDROID || UNITY_IPHONE || UNITY_WINRT || UNITY_WP8 || UNITY_BLACKBERRY
+		SelectedControl = 1;		//tablet
+		#endif
+		touchScreen = false;
+		#if UNITY_ANDROID
+		touchScreen = true;
+		#endif
+		#if UNITY_IOS
+		touchScreen = true;
+		#endif
+	}
+
+	static public int ControlMethod( ) {
+		SelectedControl = 0;
+		#if UNITY_ANDROID || UNITY_IPHONE || UNITY_WINRT || UNITY_WP8 || UNITY_BLACKBERRY
+		SelectedControl = 1;		//tablet
+		#endif
+		touchScreen = false;
+		#if UNITY_ANDROID
+		touchScreen = true;
+		#endif
+		#if UNITY_IOS
+		touchScreen = true;
+		#endif
+
+		return( SelectedControl );
+	}
+
+	static public void EmulateTouchInterface() {
+		//this just turns on the touch interface for testing
+		SelectedControl = 1;
+	}
+}
